21adda487a97206a762ecada7bbe2eb172c6242e
Ingenieria web <fernandaumbarila@gmail.com>
2022-09-19 17:32:39 -0500

    Funciones

---
+++ JavaScript/externo.js
@@ -0,0 +1,12 @@
+function hora(){
+    let fecha;
+    fecha = new Date();
+    let cadena = fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds();
+    return cadena;
+}
+function fecha(){
+    let fecha;
+    fecha = new Date();
+    let cadena = fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear();
+    return cadena;
+}
