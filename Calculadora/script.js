f804f98d12013f7ba51dad4d19e418a539ec4e6c
María Fernanda Umbarila Suárez <fernandaumbarila@gmail.com>
2022-09-27 22:54:02 -0500

    Cambios

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,30 +1,29 @@
 const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-  };
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
   
-  function inputDigit(digit) {
+function inputDigit(digit) {
     const { displayValue, waitingForSecondOperand } = calculator;
   
     if (waitingForSecondOperand === true) {
       calculator.displayValue = digit;
       calculator.waitingForSecondOperand = false;
-    } else {
+    } 
+    else 
       calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    }
-  }
+}
   
-  function inputDecimal(dot) {
+function inputDecimal(dot) {
     // If the `displayValue` does not contain a decimal point
-    if (!calculator.displayValue.includes(dot)) {
+    if (!calculator.displayValue.includes(dot)) 
       // Append the decimal point
       calculator.displayValue += dot;
-    }
-  }
+}
   
-  function handleOperator(nextOperator) {
+function handleOperator(nextOperator) {
     const { firstOperand, displayValue, operator } = calculator
     const inputValue = parseFloat(displayValue);
   
@@ -35,7 +34,8 @@ const calculator = {
   
     if (firstOperand == null) {
       calculator.firstOperand = inputValue;
-    } else if (operator) {
+    } 
+    else if (operator) {
       const currentValue = firstOperand || 0;
       const result = performCalculation[operator](currentValue, inputValue);
   
@@ -45,9 +45,9 @@ const calculator = {
   
     calculator.waitingForSecondOperand = true;
     calculator.operator = nextOperator;
-  }
+}
   
-  const performCalculation = {
+const performCalculation = {
     '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
   
     '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
@@ -57,60 +57,51 @@ const calculator = {
     '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
   
     '=': (firstOperand, secondOperand) => secondOperand
-  };
+};
   
-  function resetCalculator() {
+function resetCalculator() {
     calculator.displayValue = '0';
     calculator.firstOperand = null;
     calculator.waitingForSecondOperand = false;
     calculator.operator = null;
-  }
+}
   
-  function updateDisplay() {
+function updateDisplay() {
     const display = document.querySelector('.calculator-screen');
     display.value = calculator.displayValue;
+}
+  
+updateDisplay();
+  
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+  if (!target.matches('button')) {
+    return;
   }
-  
-  updateDisplay();
-  
-  const keys = document.querySelector('.calculator-keys');
-  keys.addEventListener('click', (event) => {
-    const { target } = event;
-    if (!target.matches('button')) {
-      return;
-    }
-  
-    if (target.classList.contains('operator')) {
-      handleOperator(target.value);
-          updateDisplay();
-      return;
-    }
-  
-    if (target.classList.contains('decimal')) {
-      inputDecimal(target.value);
-          updateDisplay();
-      return;
-    }
-  
-    if (target.classList.contains('all-clear')) {
-      resetCalculator();
-          updateDisplay();
-      return;
-    }
-  
+    
+  if (target.classList.contains('operator')) {
+    handleOperator(target.value);
+        updateDisplay();
+    return;
+  }
+    
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+        updateDisplay();
+    return;
+  }
+    
+  if (target.classList.contains('all-clear')) {
+    resetCalculator();
+        updateDisplay();
+    return;
+  }
+
     inputDigit(target.value);
     updateDisplay();
-  });
+});
 
-  function hora(){
-    let fecha;
-    fecha = new Date();
-    let cadena = fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds();
-    return cadena;
-}
-function fecha(){
-    let fecha;
-    fecha = new Date();
-    let cadena = fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear();
-    return cadena;
-}
+  
+
+
